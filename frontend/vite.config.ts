e77f5cc92628d0f4f26ba80a6fc29459312dbc79
Josh Holman <josh@jolman.me>
2025-09-03 10:59:24 -0700

    fix frontend build (#2)
    * fix npm run build?
    
    * swap deprecated TanStackRouterVite with tanstackRouter
    
    ---------
    
    Co-authored-by: Chom <112128328+tomasohCHOM@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,22 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
