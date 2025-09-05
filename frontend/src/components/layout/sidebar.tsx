038787d794957553f0e22c1f856ce2aecb649412
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-09-04 21:17:21 -0700

    feat: add sidebar component

---
+++ frontend/src/components/layout/sidebar.tsx
@@ -0,0 +1,79 @@
+import {
+  Calendar,
+  File,
+  FolderClock,
+  HelpCircle,
+  MessageSquareDot,
+  NotebookTabs,
+} from 'lucide-react'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+const navItems = [
+  {
+    title: 'Notes',
+    url: '/notes',
+    icon: NotebookTabs,
+  },
+  {
+    title: 'Chat',
+    url: '/chat',
+    icon: MessageSquareDot,
+  },
+  {
+    title: 'Planner',
+    url: '/planner',
+    icon: Calendar,
+  },
+  {
+    title: 'Manager',
+    url: '/manager',
+    icon: FolderClock,
+  },
+  {
+    title: 'Files',
+    url: '/files',
+    icon: File,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="px-2 py-4">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Navigation</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-2">
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarContent className="ml-auto">
+          <HelpCircle size={16} />
+        </SidebarContent>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,11 +2,11 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanstackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/layout/sidebar'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -15,8 +15,10 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-      <Header />
-      <Outlet />
+      <SidebarProvider>
+        <AppSidebar />
+        <Outlet />
+      </SidebarProvider>
       <TanstackDevtools
         config={{
           position: 'bottom-left',
