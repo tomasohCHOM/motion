fdcc928e12d4b747ceaf72e3a5d8c9bcfdc700e6
Josh Holman <josh@jolman.me>
2025-09-07 12:08:11 -0700

    comment out dupliacted provider?

--- infrastructure/development/providers.tf
+++ infrastructure/development/providers.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    vercel = {
-      source  = "vercel/vercel"
-      version = "~> 3.0"
-    }
+    #   vercel = {
+    #     source  = "vercel/vercel"
+    #     version = "~> 3.0"
+    #   }
   }
 }
 
@@ -16,6 +16,6 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-provider "vercel" {
-  api_token = var.vercel_api_token
-}
+# provider "vercel" {
+#   api_token = var.vercel_api_token
+# }
