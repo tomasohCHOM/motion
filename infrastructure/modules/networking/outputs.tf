cc42fa9f880500de9fc2ee05227a02092662b1ae
josh <joshuaholman5@gmail.com>
2025-09-12 00:19:44 -0700

    comment stuff out

--- infrastructure/modules/networking/outputs.tf
+++ infrastructure/modules/networking/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "private_subnet_ids" {
-  value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
-}
-
-output "default_security_group_id" {
-  value = aws_security_group.default.id
-}
+# output "private_subnet_ids" {
+#   value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+# }
+#
+# output "default_security_group_id" {
+#   value = aws_security_group.default.id
+# }
