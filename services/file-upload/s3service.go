35bcd9835a3fe7af012339fe349f580b3bd58e7b
josh <joshuaholman5@gmail.com>
2025-09-16 15:01:10 -0700

    wip

--- services/file-upload/s3service.go
+++ services/file-upload/s3service.go
@@ -16,6 +16,48 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+type S3Service struct {
+	client     *s3.Client
+	bucketName string
+}
+
+func NewS3Service(cfg S3Config) (*S3Service, error) {
+	client, err := CreateS3Client(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	service := &S3Service{
+		client:     client,
+		bucketName: cfg.BucketName,
+	}
+
+	// Create bucket if it doesn't exist (for MinIO)
+	if cfg.Endpoint != "" {
+		err = service.CreateBucketIfNotExists()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return service, nil
+}
+
+func (s *S3Service) createBucketIfNotExists() error {
+	_, err := s.client.HeadBucket(context.TODO(), &s3.HeadBucketInput{
+		Bucket: &s.bucketName,
+	})
+
+	if err != nil {
+		_, err = s.client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+			Bucket: &s.bucketName,
+		})
+		return err
+	}
+
+	return nil
+}
+
 func uploadFile(client *s3.Client, bucket, key string, data []byte) error {
 	_, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
 		Bucket: &bucket,
