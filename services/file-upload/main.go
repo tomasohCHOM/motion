ae357c9c47924ba68ef6d0b41794ceadf0f810d5
Josh Holman <josh@jolman.me>
2025-09-18 14:48:56 -0700

    fixup main errors

--- services/file-upload/main.go
+++ services/file-upload/main.go
@@ -4,13 +4,11 @@ import (
 	"log"
 )
 
-
-
 func main() {
 
 	client, err := CreateS3Client(GetS3Config())
 	if err != nil {
-		log.Fatalf("unable to create S3 service", err)
+		log.Fatalf("unable to create S3 service: %v", err)
 	}
 
 	_ = client
