a3bb52323ccbcffb5bd93c690137bf32141a4e7e
Josh Holman <josh@jolman.me>
2025-09-23 16:27:09 -0700

    complete s3cleint implementation

--- services/file-upload/internal/storage/s3.go
+++ services/file-upload/internal/storage/s3.go
@@ -2,8 +2,10 @@ package storage
 
 import (
 	"context"
+	"io"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -28,3 +30,80 @@ func (s *S3Client) GeneratePresignedURL(ctx context.Context, bucket, key string,
 	}
 	return req.URL, nil
 }
+func (s *S3Client) PutObject(ctx context.Context, bucket, key string, data io.Reader, size int64) error {
+	_, err := s.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:        aws.String(bucket),
+		Key:           aws.String(key),
+		Body:          data,
+		ContentLength: aws.Int64(size),
+	})
+	return err
+}
+
+func (s *S3Client) DeleteObject(ctx context.Context, bucket, key string) error {
+	_, err := s.client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	return err
+}
+
+func (s *S3Client) GetObjectURL(ctx context.Context, bucket, key string) (string, error) {
+	presignClient := s3.NewPresignClient(s.client)
+	req, err := presignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = 1 * time.Hour // 1 hour expiry for GET URLs
+	})
+	if err != nil {
+		return "", err
+	}
+	return req.URL, nil
+}
+
+// Optional: Additional methods for extended functionality
+func (s *S3Client) GetObject(ctx context.Context, bucket, key string) (io.ReadCloser, error) {
+	result, err := s.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return result.Body, nil
+}
+
+func (s *S3Client) HeadObject(ctx context.Context, bucket, key string) (*s3.HeadObjectOutput, error) {
+	return s.client.HeadObject(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+}
+
+func (s *S3Client) ListObjects(ctx context.Context, bucket, prefix string) ([]string, error) {
+	var keys []string
+
+	paginator := s3.NewListObjectsV2Paginator(s.client, &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(prefix),
+	})
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, obj := range page.Contents {
+			if obj.Key != nil {
+				keys = append(keys, *obj.Key)
+			}
+		}
+	}
+
+	return keys, nil
+}
+
+// Compile-time interface compliance check
+var _ StorageClient = (*S3Client)(nil)
