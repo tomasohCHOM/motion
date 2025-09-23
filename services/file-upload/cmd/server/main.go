8032c545352c70ca6de74bf09e853c10472c0569
Josh Holman <josh@jolman.me>
2025-09-23 16:08:22 -0700

    wip

--- services/file-upload/cmd/server/main.go
+++ services/file-upload/cmd/server/main.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/tomasohchom/motion/services/file-upload/internal/clients"
 	"github.com/tomasohchom/motion/services/file-upload/internal/config"
 	"github.com/tomasohchom/motion/services/file-upload/internal/handlers"
+	"github.com/tomasohchom/motion/services/file-upload/internal/services"
 )
 
 func main() {
 
 	cfg := config.Load()
+	storageClient, err := clients.NewStorageClient(cfg)
+	if err != nil {
+		log.Fatalf("Failed to create storage client:", err)
+	}
+
+	uploadService := services.NewUploadService(storageClient, cfg)
+	uploadHandler := services.NewUploadHandler(uploadService, cfg)
 	mux := http.NewServeMux()
-	// uploadHandler := handlers.NewUploadHandler(cfg)
-	// mux.HandleFunc("GET /health", handlers.HealthCheck)
-	// mux.HandleFunc("POST /health", uploadHandler.GetPresignedURL)
-	// mux.HandleFunc("POST /upload/complete", uploadHandler.CompleteUpload)
+	mux.HandleFunc("GET /health", uploadHandler.HealthCheck)
+	mux.HandleFunc("POST /health", uploadHandler.GetPresignedURL)
+	mux.HandleFunc("POST /upload/complete", uploadHandler.CompleteUpload)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
