34910186a8c6bb64575e21702b372e768c5f00a1
josh <joshuaholman5@gmail.com>
2025-09-30 10:08:34 -0700

    improve logging

--- services/file-upload/cmd/server/main.go
+++ services/file-upload/cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/tomasohchom/motion/services/file-upload/internal/clients"
 	"github.com/tomasohchom/motion/services/file-upload/internal/config"
@@ -11,14 +12,36 @@ import (
 	"github.com/tomasohchom/motion/services/file-upload/internal/services"
 )
 
+func checkMinIOConnection(cfg *config.Config) error {
+	client := &http.Client{Timeout: 3 * time.Second}
+	resp, err := client.Get(fmt.Sprintf("http://%s/minio/health/live", cfg.Storage.Endpoint))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unhealthy status: %d", resp.StatusCode)
+	}
+	return nil
+}
+
 func main() {
 
+	log.SetPrefix("[SERVER] ")
 	cfg := config.Load()
 	storageClient, err := clients.NewStorageClient(cfg)
 	if err != nil {
 		log.Fatalf("Failed to create %s storage client: %v", cfg.Storage.Provider, err)
 	}
 	log.Printf("Successfully created %s storage client\n", cfg.Storage.Provider)
+
+	if cfg.Storage.Provider == "minio" {
+		if err := checkMinIOConnection(cfg); err != nil {
+			log.Printf("MinIO connection failed: %v", err)
+		}
+	}
+	log.Println("Successfully connected to MinIO")
 
 	uploadService := services.NewUploadService(storageClient, cfg)
 	uploadHandler := handlers.NewUploadHandler(uploadService, cfg)
