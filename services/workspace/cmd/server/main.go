bded92ee6ddf690a9cfe0a31eb4f713f5aebb47a
Josh Holman <josh@jolman.me>
2025-10-01 14:38:19 -0700

    connect to db in thread

--- services/workspace/cmd/server/main.go
+++ services/workspace/cmd/server/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
+
+	"github.com/jackc/pgx/v5"
 
 	"github.com/tomasohchom/motion/services/workspace/internal/config"
 	"github.com/tomasohchom/motion/services/workspace/internal/handlers"
@@ -17,17 +19,39 @@ func main() {
 	cfg := config.Load()
 	mux := http.NewServeMux()
 
-	var err error
-	var conn *pgx.Conn
-	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	for err != nil {
-		log.Printf("WARNING: Unable to connect to database: %v\n", err)
-		log.Printf("Trying again in 5 seconds...")
-		time.Sleep(time.Second * 5)
-		conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	}
+	var (
+		conn   *pgx.Conn
+		connMu sync.RWMutex
+	)
+
+	// Start db in seperate thread
+	go func() {
+		var err error
+		for {
+			connMu.Lock()
+			conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+			connMu.Unlock()
+
+			if err == nil {
+				log.Println("Successfully connected to database")
+				defer func() {
+					connMu.Lock()
+					if conn != nil {
+						conn.Close(context.Background())
+					}
+					connMu.Unlock()
+				}()
+				break
+			}
+
+			log.Printf("WARNING: Unable to connect to database: %v\n", err)
+			log.Printf("Trying again in 5 seconds...")
+			time.Sleep(time.Second * 5)
+		}
+
+	}()
+
 	defer conn.Close(context.Background())
-	log.Println("Successfully connected to database")
 
 	mux.HandleFunc("GET /health", handlers.HealthCheck)
 
