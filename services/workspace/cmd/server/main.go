afc7be3368ebba80e7c983931801c182d5888992
Josh Holman <josh@jolman.me>
2025-09-30 17:39:16 -0700

    setup pgx

--- services/workspace/cmd/server/main.go
+++ services/workspace/cmd/server/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"context"
+	"github.com/jackc/pgx/v5"
 
 	"github.com/tomasohchom/motion/services/workspace/internal/config"
 	"github.com/tomasohchom/motion/services/workspace/internal/handlers"
@@ -13,6 +16,13 @@ import (
 func main() {
 	cfg := config.Load()
 	mux := http.NewServeMux()
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+	log.Println("Successfully connected to database")
 
 	mux.HandleFunc("GET /health", handlers.HealthCheck)
 
