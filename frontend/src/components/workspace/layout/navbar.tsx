25608ca688daae6bc4c415dc2615874bf9a3d841
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-10-02 15:05:05 -0700

    navbar: revert back to 16rem

--- frontend/src/components/workspace/layout/navbar.tsx
+++ frontend/src/components/workspace/layout/navbar.tsx
@@ -4,7 +4,7 @@ export default function AppNavbar() {
   return (
     <nav
       className="fixed top-0 right-0 left-0 flex items-center justify-between
-      border-b border-sidebar-border p-5 bg-background z-10 md:left-[var(--sidebar-length,_16rem)]"
+      border-b border-sidebar-border p-5 bg-background z-10 md:left-[16rem]"
     >
       <span className="font-semibold">CPSC 491's Workspace</span>
       <div>
