6e68d9e114ee46ec302f79f192627cf56899c95d
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-10-10 18:27:19 -0700

    feat: wire up workspace service db with user handler

--- services/workspace/go.mod
+++ services/workspace/go.mod
@@ -3,14 +3,17 @@ module github.com/tomasohchom/motion/services/workspace
 go 1.24.6
 
 require (
+	github.com/clerk/clerk-sdk-go/v2 v2.4.2
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/rs/cors v1.11.1
 )
 
 require (
+	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 )
--- services/workspace/internal/config/config.go
+++ services/workspace/internal/config/config.go
@@ -2,13 +2,13 @@ package config
 
 import (
 	"os"
-	// "strconv"
 )
 
 type Config struct {
 	Port        string
 	Environment string
 	DbEndpoint  string
+	ClerkKey    string
 }
 
 func Load() *Config {
@@ -16,6 +16,7 @@ func Load() *Config {
 		Port:        getEnv("PORT", "8081"),
 		Environment: getEnv("ENV", "development"),
 		DbEndpoint:  getEnv("DATABASE_URL", "postgres://root@database:5432"),
+		ClerkKey:    getEnv("CLERK_API_KEY", ""),
 	}
 }
 
@@ -23,16 +24,5 @@ func getEnv(key, defaultValue string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
-
 	return defaultValue
 }
-
-// func getBoolEnv(key string, defaultValue bool) bool {
-// 	if value := os.Getenv(key); value != "" {
-// 		if parsed, err := strconv.ParseBool(value); err == nil {
-// 			return parsed
-// 		}
-// 	}
-//
-// 	return defaultValue
-// }
