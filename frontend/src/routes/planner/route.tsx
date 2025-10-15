b650b8b204ba34b9e827496658bd6a5008add395
syevers <32802098+syevers@users.noreply.github.com>
2025-10-15 12:57:06 -0700

    remove files

--- frontend/src/routes/planner/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/planner')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/planner"!</div>
-}
