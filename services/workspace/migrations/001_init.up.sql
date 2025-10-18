408c68e820422277968afceb2e0232861d5af44e
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-10-17 19:01:09 -0700

    make username column unique

--- services/workspace/migrations/001_init.up.sql
+++ services/workspace/migrations/001_init.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
