a045b12194bc86a0bd1c7c4c76980b77f6652cc6
josh <joshuaholman5@gmail.com>
2025-11-04 09:38:50 -0800

    use text instead of varchar

--- services/file-upload/migrations/001.create_files.up.sql
+++ services/file-upload/migrations/001.create_files.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE files (
   id UUID PRIMARY KEY,
   user_id UUID NOT NULL,
-  original_name VARCHAR(255) NOT NULL,
-  storage_key VARCHAR(512) NOT NULL UNIQUE,
-  content_type VARCHAR(127) NOT NULL,
+  original_name TEXT NOT NULL,
+  storage_key TEXT NOT NULL UNIQUE,
+  content_type TEXT NOT NULL,
   size_bytes BIGINT NOT NULL,
   uploaded_at TIMESTAMPTZ DEFAULT NOW(),
   metadata JSONB DEFAULT '{}'::jsonb,
