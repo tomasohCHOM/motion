05c82a78a52fb854508f3d61841c9a8d24edc459
josh <joshuaholman5@gmail.com>
2025-11-11 16:36:55 -0800

    ping postgres container

--- services/file-upload/tests/integration/setup_test.go
+++ services/file-upload/tests/integration/setup_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/minio"
@@ -16,13 +16,14 @@ import (
 type TestSuite struct {
 	suite.Suite
 	ctx            context.Context
-	db             *pgx.Conn
+	pool           *pgxpool.Pool
 	pgContainer    *postgres.PostgresContainer
 	minioContainer *minio.MinioContainer
 }
 
 func (ts *TestSuite) SetupSuite() {
 	ts.ctx = context.Background()
+	// Postgres
 	pgContainer, err := postgres.Run(
 		ts.ctx,
 		"postgres:18.0",
@@ -36,11 +37,26 @@ func (ts *TestSuite) SetupSuite() {
 	)
 	ts.NoError(err)
 
-	// conn, err := pgx.Connect(ts.ctx)
-	// ts.NoError(err)
+	host, err := pgContainer.Host(ts.ctx)
+	ts.NoError(err)
+
+	port, err := pgContainer.MappedPort(ts.ctx, "5432")
+	ts.NoError(err)
+
+	dsn := fmt.Sprintf("postgres://testuser:testpw@%s:%s/testdb?sslmode=disable",
+		host, port.Port())
+	pool, err := pgxpool.New(ts.ctx, dsn)
+	if err == nil {
+		err = pool.Ping(ts.ctx)
+	}
+	pool.Close()
+	ts.NoError(err, "Failed to ping Postgres container")
+
+	ts.NoError(err)
 
 	ts.pgContainer = pgContainer
 
+	// Minio
 	minioContainer, err := minio.Run(
 		ts.ctx,
 		"minio:latest",
