3bbea4c72cf77d4f6a0c26d3349c15b3b9c6fd35
Josh Holman <josh@jolman.me>
2025-11-12 20:50:15 -0800

    file-upload: Add SQL linting to CI with SQLFluff (#107)
    * add sqlfluff locally
    
    * sqlfluff fix
    
    * resolve AM04 errors
    
    * resolve line length warnings
    
    * add to github action

--- services/file-upload/sqlc/queries/files.sql
+++ services/file-upload/sqlc/queries/files.sql
@@ -1,26 +1,63 @@
 -- name: CreateFile :one
 INSERT INTO files (
-  id, user_id, original_name, storage_key, content_type, size_bytes, metadata
+    id, user_id, original_name, storage_key, content_type, size_bytes, metadata
 ) VALUES (
-
-  $1, $2, $3, $4, $5, $6, $7
-) RETURNING *;
+    $1, $2, $3, $4, $5, $6, $7
+) RETURNING
+    id, user_id, original_name, storage_key, content_type, size_bytes,
+    metadata, uploaded_at;
 
 -- name: GetFileById :many
-SELECT * FROM files
+SELECT
+    id,
+    user_id,
+    original_name,
+    storage_key,
+    content_type,
+    size_bytes,
+    metadata,
+    uploaded_at
+FROM files
 WHERE id = $1;
 
 -- name: ListFiles :many
-SELECT * FROM files;
+SELECT
+    id,
+    user_id,
+    original_name,
+    storage_key,
+    content_type,
+    size_bytes,
+    metadata,
+    uploaded_at
+FROM files;
 
 -- name: ListFilesByUser :many
-SELECT * FROM files
+SELECT
+    id,
+    user_id,
+    original_name,
+    storage_key,
+    content_type,
+    size_bytes,
+    metadata,
+    uploaded_at
+FROM files
 WHERE user_id = $1
 ORDER BY uploaded_at DESC
 LIMIT $2 OFFSET $3;
 
 -- name: SearchFilesByMetadata :many
-SELECT * FROM files
+SELECT
+    id,
+    user_id,
+    original_name,
+    storage_key,
+    content_type,
+    size_bytes,
+    metadata,
+    uploaded_at
+FROM files
 WHERE user_id = $1 AND metadata @> $2::jsonb
 ORDER BY uploaded_at DESC;
 
